4e757dc7da7556ddca957e88341c09991d274d92
hertsjsy <sumanherts@gmail.com>
2023-07-27 00:42:12 +0100

    sign in form

--- workplace/src/app/app-routing.module.ts
+++ workplace/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
-
+import { RegisterComponent } from './components/auth/register/register.component';
+RegisterComponent
 
 const routes: Routes = [
-  {path:"login",component:LoginComponent},
+  {path:"login",
+  component:LoginComponent},
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
   
 ];
 
--- workplace/src/app/components/auth/login/login.component.ts
+++ workplace/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormBuilder,FormControl,Validators} from"@angular/forms"
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,5 +11,31 @@ import { Component } from '@angular/core';
  
 })
 export class LoginComponent {
+  loginForm:FormGroup
+  submitted = false;
+  constructor(
+    private  formBuilder:FormBuilder,
+    private router: Router,
+  ) { }
 
+  ngOnInit(): void {
+  this.createLoginForm();
+}
+  createLoginForm(){
+    this.loginForm=this.formBuilder.group({
+      email:["",Validators.required],
+      password:["",Validators.required]
+    })
+  }
+
+
+  onSubmit(){
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.loginForm.value);
+    
+    }
+   
+  }
 }
